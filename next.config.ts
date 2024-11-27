f9d17532b785b187f18b2e3721a088fd2604c9ba
zhangtao25 <wr.zhang25@gmail.com>
2024-11-27 11:30:31 +0800

    upd

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    experimental:{
+        swcPlugins:[
+            ['swc-plugin-coverage-instrument',{}],
+            ['swc-plugin-canyon',{}]
+        ]
+    }
 };
 
 export default nextConfig;
